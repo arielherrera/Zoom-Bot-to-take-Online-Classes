ed701047d96445bc47cef26114aa363a47f301f0
arielherrera <>
2021-02-03 13:38:51 -0600

    zoom_bot.py modified

--- zoom_bot.py
+++ zoom_bot.py
@@ -1,25 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import datetime
 import time
 import keyboard
 class zoom_bot:
 	def join(self, meeting_link):
-		self.bot = webdriver.Chrome("chromedriver.exe")
-		self.bot.get(meeting_link)
-		time.sleep(5)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('enter', do_press=True, do_release=True)
-		time.sleep(5)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('tab', do_press=True, do_release=True)
-		keyboard.send('enter', do_press=True, do_release=True)
-
-		self.bot.quit()
-
+		try:
+			driver = webdriver.Firefox()
+			driver.get(meeting_link)
+			driver.current_window_handle
+			time.sleep(3)
+			element = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/div[1]/div')
+			driver.switch_to.frame(frame)
+			driver.find_element(By.TAG_NAME, 'button').click()
+		finally:
+			driver.quit()
+			# quit will:
+			# close all windows and tabs associated with that Webdriver session
+			# close the broswer process
+			# close the background driver process
 
 link = open("meeting_link.txt","r").read()
-obj = zoom_bot()
-obj.join(link)
+zoom_bot().join(link)
